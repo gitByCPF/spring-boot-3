b0645b122a234e4013951a0e857fbc4ee754d979
cpf <cpfprogrammer@163.com>
2025-05-11 03:51:19 +0800

    整理初始模块

--- boot3-03-logging/src/main/java/com/cpf/logging/LoggingApplication.java
+++ boot3-03-logging/src/main/java/com/cpf/logging/LoggingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Boot303LoggingApplication {
+public class LoggingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Boot303LoggingApplication.class, args);
+        SpringApplication.run(LoggingApplication.class, args);
     }
 
 }
